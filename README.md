355d04d85e3b86968f87738b59f89cc105ff6bd8
Arthur <jiehua.zhang@hotmail.com>
2020-04-05 23:38:28 -0400

    initial commit with correct readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rust-learning-chatper-2
+What
+---------------
+This is practice of RUST chapter 2 - creating a guessing game with RUST. In this chapter I learned how to match(compare) two integeters and very basic exception handling. 
+
+Why the code format is so ugly
+------------------------------
+I have not yet learned how to format things with EMACS. I am basically learning two things at the sametime (I am not sure if this is a good idea or not). I am learning EMACS as well as RUST. Anyway the code works so far. The format will be fixed later. sorry. 
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    
+         println!("Guess the number!");
+
+         let secret_number = rand::thread_rng().gen_range(1, 101);  
+
+             println! ("secret number: {}", secret_number);
+            loop {
+                 println!("Please input your guess.");
+
+                     let mut guess = String::new();
+
+                         io::stdin().read_line(&mut guess)
+                                 .expect("failed to readline");
+
+                                     println!("You guessed: {}", guess);
+    
+                    let  guess: u32 = match guess.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => { println!("Please input an valid number");
+                           continue
+                           },
+        };
+               
+
+   match  guess.cmp(&secret_number) {
+       Ordering::Less => println! ("Too small!"),
+       Ordering::Greater =>  println! ("Too big!"),
+       Ordering::Equal =>  {
+             println! ("You win!");
+             break;
+             }
+    }
+    }
+}
